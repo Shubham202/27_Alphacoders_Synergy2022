c04a1ff243baebd344b00f48037c356399bbfa28
Shubham202 <51954852+Shubham202@users.noreply.github.com>
2022-10-22 10:09:46 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -37,6 +37,43 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+app.get("/admin", (req, res) => {
+    res.render("index");
+});
+
+app.post("/register_student", (req, res) => {
+    const student = new Student({
+        Name: req.body.name,
+        Email: req.body.emailID,
+        Password: req.body.password,
+        Contact_number: req.body.contactNumber,
+        Roll_number: req.body.rollNumber
+    });
+
+    Student.findOne({ Email: req.body.emailID }, (err, foundStudent) => {
+        if (!foundStudent) {
+            student.save();
+            res.render("login");
+        } else {
+            let message = "Student already exist";
+            res.render("register", { message: message });
+        }
+    });
+});
+
+app.post("/login_student", (req, res) => {
+    Student.findOne({ Email: req.body.emailID }, (err, foundStudent) => {
+        if (foundStudent) {
+            if (req.body.password === foundStudent.Password) {
+                res.render("GetBloodPage", { banks: null });
+            }
+        } else {
+            let message = "Student does not exist";
+            res.render("register", { message: message });
+        }
+    });
+});
+
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
